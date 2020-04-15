1b7bf12626a7913c668ede642c20c83f1a070cbb
Jorge Gomez <jorge6242@gmail.com>
2020-04-15 19:21:37 -0400

    default settins

--- database/seeds/CreateDefaultSystemParamsSeeder.php
+++ database/seeds/CreateDefaultSystemParamsSeeder.php
@@ -14,44 +14,42 @@ class CreateDefaultSystemParamsSeeder extends Seeder
     public function run()
     {
         $data = [
-            [ 
                 [
-                    'description' => 'Cliente Deno',
+                    'description' => 'Nombre del cliente',
                     'parameter' => 'CLIENT_NAME',
-                    'value' => 1,
+                    'value' => 'Cliente Demo',
                     'eliminable' => 1,
                 ],
                 [
                     'description' => 'logoweb.jpg',
                     'parameter' => 'CLIENT_LOGO',
-                    'value' => 1,
+                    'value' => 'logoweb.jpg',
                     'eliminable' => 1,
                 ],
                 [
-                    'description' => '0',
+                    'description' => 'Sitio offline',
                     'parameter' => 'SITE_OFFLINE',
-                    'value' => 1,
+                    'value' => '0',
                     'eliminable' => 1,
                 ],
                 [
-                    'description' => '1.1.0',
+                    'description' => 'Version Base de datos',
                     'parameter' => 'DB_VERSION',
-                    'value' => 1,
+                    'value' => '1.1.0',
                     'eliminable' => 1,
                 ],
                 [
-                    'description' => '1.1.1',
+                    'description' => 'Version Interfaz',
                     'parameter' => 'FRONTEND_VERSION',
-                    'value' => 1,
+                    'value' => '1.1.1',
                     'eliminable' => 1,
                 ],
                 [
-                    'description' => '1.1.2',
+                    'description' => 'Version Backend',
                     'parameter' => 'BACKEND_VERSION',
-                    'value' => 1,
+                    'value' => '1.1.2',
                     'eliminable' => 1,
                 ]
-            ],
         ];
         foreach ($data as $element) {
             Parameter::create([
