d275070ff0e6ac2693f792cdc0be4a942c5884a7
Jorge Gomez <jorge6242@gmail.com>
2020-04-28 19:13:22 -0400

    update parameter

--- database/seeds/CreateDefaultSystemParamsSeeder.php
+++ database/seeds/CreateDefaultSystemParamsSeeder.php
@@ -64,7 +64,7 @@ class CreateDefaultSystemParamsSeeder extends Seeder
                 ],                [
                     'description' => 'Servidor de Base de datos',
                     'parameter' => 'DB_SERVER',
-                    'value' => 'SQL SERVER',
+                    'value' => 'http://192.168.0.11',
                     'eliminable' => 1,
                 ]
         ];
